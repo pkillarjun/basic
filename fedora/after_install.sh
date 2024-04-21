640b840576e6c0d536e53e1f62ae8251543a0674
Arjun <pkillarjun@protonmail.com>
2024-04-21 18:07:48 +0530

    [fedora] setup
    Signed-off-by: Arjun <pkillarjun@protonmail.com>

---
+++ fedora/after_install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Update fedora core
+sudo dnf -y update
+
+# Install basic utility
+sudo dnf install -y neovim vim-common neofetch btop wireshark tcpdump python3-pwntools chromium
+sudo dnf install -y podman podman-compose podman-docker
+
+# Install dev toolchain
+sudo dnf install -y clang clang-devel llvm llvm-devel compiler-rt clang-tools-extra libcxx-devel libcxx-static
+sudo dnf install -y autoconf automake cmake libtool
+sudo dnf install -y libasan libubsan libtsan libhwasan liblsan
+sudo dnf install -y ragel bison flex git
+sudo dnf groupinstall -y "Development Tools" "C Development Tools and Libraries"
+sudo dnf install -y golang lua rust cargo rustfmt
+
+# Install vscode [Sorry stallman]
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install -y code
+
+# Oh My Bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# Neovim base conf
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
+
+# bashrc conf
+mkdir -p $HOME/.go
+echo '# conf'                   >> ~/.bashrc
+echo 'export GOPATH=$HOME/.go'  >> ~/.bashrc
+echo "alias vi=nvim"            >> ~/.bashrc
---
+++ fedora/init.vim
@@ -0,0 +1,16 @@
+set number
+set relativenumber
+
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+set ignorecase
+set smartcase
+set hlsearch
+
+filetype plugin on
+syntax on
+
+" Set back cursor
+autocmd VimLeave * set guicursor=a:hor20-blinkon250
