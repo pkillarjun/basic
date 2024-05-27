97e8085d972ba07c2f76a0518992fedb7e7f2890
Arjun <167687092+pkillarjun@users.noreply.github.com>
2024-05-27 10:23:07 +0530

    Update after_install.sh

--- fedora/after_install.sh
+++ fedora/after_install.sh
@@ -47,5 +47,4 @@ mkdir -p $HOME/.go
 echo '# conf'                   >> ~/.bashrc
 echo 'export GOPATH=$HOME/.go'  >> ~/.bashrc
 echo "alias vi=nvim"            >> ~/.bashrc
-alias rm="gio trash"
-
+echo 'alias rm="gio trash"'     >> ~/.bashrc
